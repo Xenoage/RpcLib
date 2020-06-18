ac83f0ef149ea9a136e1a60b8cc71a195fa6e4c4
Andreas Wenger <andi@xenoage.com>
2020-06-18 21:21:18 +0200

    Restructuring demo projects

---
+++ Demos/RetryTest/DemoShared/Rpc/ICalcRpc.cs
@@ -0,0 +1,27 @@
+﻿using RpcLib;
+using System.Threading.Tasks;
+
+namespace DemoShared.Rpc {
+
+    /// <summary>
+    /// Demo interface for some additional calculation <see cref="RpcFunctions"/>, both client-side and server-side.
+    /// This demonstrates, that
+    /// * multiple interfaces can be registered to distribute the registered
+    ///   functions over several interfaces/classes
+    /// * the same interfaces/classes can be used both on the client side and the server side
+    /// </summary>
+    public interface ICalcRpc : IRpcFunctions {
+
+        /// <summary>
+        /// Returns the sum of the given two numbers.
+        /// </summary>
+        Task<int> AddNumbers(int number1, int number2);
+
+        /// <summary>
+        /// Returns the quotient of the given two numbers.
+        /// </summary>
+        Task<int> DivideNumbers(int dividend, int divisor);
+
+    }
+
+}
