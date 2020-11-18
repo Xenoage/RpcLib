4563f9ae4195cfc7a858ddc6288ddef0c903502d
Andreas Wenger <andi@xenoage.com>
2020-11-18 19:46:47 +0100

    ToDo list added

---
+++ RpcLib/ToDos.cs
@@ -0,0 +1,9 @@
+﻿/**********
+
+List of TODOs
+*************
+
+- Prevent CSWSH (Cross-Site WebSocket Hijacking) by using a whitelist of the allowed origins.
+  See: https://www.codetinkerer.com/2018/06/05/aspnet-core-websockets.html
+
+**********/
