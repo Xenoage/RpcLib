60967dd0524f565956084ad005680698cb85b128
Andreas Wenger <andi@xenoage.com>
2020-11-18 16:26:23 +0100

    Project started; auth on server

---
+++ RpcLib/Auth/IRpcServerAuth.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Xenoage.RpcLib.Auth {
+
+    /// <summary>
+    /// Client authentication on the server. Based on a given HTTP request,
+    /// the implementing class must identify the user or return an authentication failure.
+    /// This can be done by using a Basic Auth header for example, as demonstrated
+    /// in RpcServerBasicAuth.
+    /// </summary>
+    public interface IRpcServerAuth {
+
+        /// <summary>
+        /// Tries to authenticate the client by the given HTTP request.
+        /// </summary>
+        AuthResult Authenticate(HttpListenerRequest request);
+
+    }
+
+}
---
+++ RpcLib/Logging/ILogger.cs
@@ -0,0 +1,17 @@
+﻿namespace Xenoage.RpcLib.Logging {
+
+    /// <summary>
+    /// Logging interface for the RpcLib library.
+    /// It is intentially kept very simple, so that any logging framework can easily
+    /// be adapted to receive log messages from this library.
+    /// </summary>
+    public interface ILogger {
+
+        /// <summary>
+        /// Logs the given message with the given severity.
+        /// </summary>
+        public void Log(string message, LogLevel level);
+
+    }
+
+}
