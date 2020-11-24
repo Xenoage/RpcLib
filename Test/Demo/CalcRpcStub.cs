da15eae44e2147726ec5778c3d23f18bdd0e1775
Andreas Wenger <andi@xenoage.com>
2020-11-24 20:44:32 +0100

    ICalcRpc demo implementation

---
+++ Test/Demo/CalcRpcStub.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Xenoage.RpcLib.Methods;
+
+namespace Xenoage.RpcLib.Demo {
+
+    /// <summary>
+    /// Demo stub implementation of the <see cref="ICalcRpc"/> methods.
+    /// This is the class used on the calling peer, i.e. it contains code to call the remote side.
+    /// This is just boilerplate code; we could auto-generate this class later in .NET 5 with source generators.
+    /// </summary>
+    public class CalcRpcStub : RpcMethodsStub, ICalcRpc {
+
+        public CalcRpcStub(string? targetPeerID) : base(targetPeerID) {
+        }
+
+        public Task<int> AddNumbers(int number1, int number2) =>
+            ExecuteOnRemotePeer<int>("AddNumbers", number1, number2);
+
+        public Task<int> DivideNumbers(int dividend, int divisor) =>
+            ExecuteOnRemotePeer<int>("DivideNumbers", dividend, divisor);
+    }
+}
---
+++ Test/Demo/ICalcRpc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Xenoage.RpcLib.Demo {
+
+    /// <summary>
+    /// Demo interface for some calculation <see cref="IRpcMethods"/>.
+    /// </summary>
+    public interface ICalcRpc : IRpcMethods {
+
+        /// <summary>
+        /// Returns the sum of the given two numbers.
+        /// </summary>
+        Task<int> AddNumbers(int number1, int number2);
+
+        /// <summary>
+        /// Returns the quotient of the given two numbers.
+        /// Throws a <see cref="DivideByZeroException"/> when the divisor is 0.
+        /// </summary>
+        Task<int> DivideNumbers(int dividend, int divisor);
+
+    }
+
+}
